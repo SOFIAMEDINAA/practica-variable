651830e4bf541d475b29d021903a6140546a5b03
Sofia Medina <medinasofiaa1@gmail.com>
2023-04-20 12:30:07 -0300

    practica variables

---
+++ logaritmo.js
@@ -0,0 +1,20 @@
+let caracterInicialPais = "a";
+let menasajeReunion = "HOLA";
+let pesoBotellaLitro = 5;
+let estadoServicio = true;
+let nombreCFP = "CFP27";
+let contraseniaYoutube = "tyciytco1"
+let numCelular = "11984984984";
+let nombrePostre = "tiramisu"
+let claveProducto = 981-165151-5251;
+
+/* Document.write("<h1> caracter:" + inicialPais + "</h1>");
+Document.write("<h1> palabra:" + menasajeReunion + "</h1>");
+Document.write("<h1> decimal:" + pesoBotellaAgua + "</h1>");
+Document.write("<h1> palabra:" + estadoServicioTransporte + "</h1>");
+Document.write("<h1> palabra:" + nombreCentro + "</h1>");
+Document.write("<h1> carcater:" + contraseñaYoutube + "</h1>");
+Document.write("<h1> entero:" + numCelular + "</h1>");
+Document.write("<h1> palabra:" + nombrePostre + "</h1>");
+Document.write("<h1> caracter:" + claveProducto + "</h1>"); */
+
